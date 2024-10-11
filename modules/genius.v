695076d00dc4b65a0268ae96a58aa6896bb206ef
lucascalumbi <lucascalumbi30@gmail.com>
2024-10-11 15:30:29 -0300

    grandes atualizacoes, grandes responsabilidades

--- modules/genius.v
+++ modules/genius.v
@@ -5,16 +5,13 @@ module genius(
   input reset,
   input start, 
   input [9:2] sw,
+//begin1
   output reg [6:0] segd0,  
   output reg [6:0] segd1,  
   output reg [6:0] segd2,  
   output reg [6:0] segd3, 
   output reg [9:0] leds    
 );
-
-
-parameter true = 1'b1;
-parameter false = 1'b0;
 
 reg [3:0] sequence_count;
 wire [1:0] current_number;
@@ -26,8 +23,8 @@ dec7seg_2bits dec7_2bits(.x(segd_0), .a(current_number));
 reg [3:0] current_level;
 wire [6:0] segd_2;
 wire [6:0] segd_3;
-dec7seg_4bits_hexadec dec7seg_4bits_hexadec0(.y(segd_3), .a(current_level));
-dec7seg_4bits_hexadec dec7seg_4bits_hexadec1(.y(segd_2), .a(sequence_count));
+dec7seg_4bits_hexadec dec7seg_4bits_hexadec0(.y(segd_3), .a(current_level)); // temporario
+dec7seg_4bits_hexadec dec7seg_4bits_hexadec1(.y(segd_2), .a(sequence_count)); // temporario
 //dec7seg_4bits_1x2 dec7_4bits_1x2(.x(segd_3), .y(segd_2), .a(current_level));
 
 wire is_right_choice;
@@ -40,13 +37,18 @@ wire [9:0] shifted_leds;
 shift_leds shift(.y(shifted_leds), .x(leds));
 
 reg [2:0] state, next_state;
-  // estados da FSM
-  parameter reset_game_state = 3'o0;
-  parameter show_sequence_state = 3'o1;
-  parameter receive_inputs_state = 3'o2;
-  parameter add_difficult_state = 3'o3;
+reg clock_count;
 
-  parameter seg_off = 7'b0000000;
+// estados da FSM
+parameter reset_game_state = 3'o0;
+parameter show_sequence_state = 3'o1;
+parameter receive_inputs_state = 3'o2;
+parameter add_difficult_state = 3'o3;
+
+parameter seg_off = 7'b0000000;
+parameter leds_off = 10'b0000000000;
+parameter leds_on = 10'b1111111111;
+
 
 always @(posedge clock) begin
   if(!reset) begin
@@ -57,27 +59,27 @@ always @(posedge clock) begin
 
       case (state)
         reset_game_state: begin
-          leds <= 10'b1111111111;
+          leds <= leds_on;
           segd0 <= seg_off;
           next_state <= state; // mantenha o estado atual
           // Resetar o jogo
           if (start) begin 
             sequence_count <= 4'h0;
             current_level <= 4'h0;
-            leds <= 10'b0000000001;
+            leds <= 10'h1;
             next_state <= show_sequence_state; // mostre a sequencia de numeros
           end
         end
 
         show_sequence_state: begin
-          if (sequence_count > current_level) begin
-            segd0 <= seg_off;
-            leds <= 10'b0000000001;
+            segd0 <= segd_0;
+            clock_count <= 1'b0;
+          if (sequence_count >= current_level) begin
+            leds <= 10'h1;
             sequence_count <= 4'h0;
             next_state <= receive_inputs_state;
           end
           else begin
-            segd0 <= segd_0;
             sequence_count <= sequence_count + 1'b1;
             leds <= shifted_leds;
             next_state <= state; // mantenha o estado atual
@@ -92,13 +94,12 @@ always @(posedge clock) begin
           else begin 
             if(was_some_btn_pressed) begin
               if(is_right_choice) begin
-                //leds <= 10'b1111111111;
                 leds <= shifted_leds;
                 sequence_count <= sequence_count + 1'b1;
                 next_state <= state; // mantenha o estado atual
               end 
               else begin
-                leds <= 10'b0000000000;
+                leds <= leds_off;
                 next_state <= reset_game_state; // jogador perdeu, resete o jogo
               end
             end
@@ -111,10 +112,14 @@ always @(posedge clock) begin
         add_difficult_state: begin
           // Aumente a sequência
           segd0 <= seg_off;
-          if (current_level < 15) begin 
+          if (current_level < 15 && clock_count == 0) begin 
+              clock_count <= clock_count + 1'b1;
               current_level <= current_level + 1'b1;
-              sequence_count <= 1'h0;
-              next_state <= show_sequence_state; // mostre a sequencia incrementada de numeros
+              next_state <= state; // mantem no mesmo estado
+          end
+          else if (current_level < 15 && clock_count == 1) begin 
+              sequence_count <= 4'h0;
+              next_state <= show_sequence_state;
           end
           else begin
               next_state <= reset_game_state; // jogador ganhou, resete o jogo
@@ -122,17 +127,18 @@ always @(posedge clock) begin
         end 
 
         default: begin
-          leds <= 10'b0000000000;
+          leds <= leds_off;
           next_state <= reset_game_state;     
         end 
       endcase
     end
     else begin
       segd1 <= seg_off;
-      leds <= 10'b0000000000;
+      leds <= leds_off;
       segd2 <= seg_off;      
       segd3 <= seg_off;
     end
+//begin2
   end
 endmodule
 
--- programa_boboca.cpp
+++ programa_boboca.cpp
@@ -3,69 +3,153 @@
 
 using namespace std;
 
-int main(){
-
-    // rodar o programa para """recompilar""" o arquivo main.v
-    // g++ programa_boboca.cpp -o programa_boboca_executavel && ./programa_boboca_executavel
-
-    ofstream main("test_banch/main.v");
+void build_file_simulator(ofstream* file){
     string line;
 
     ifstream genius("modules/genius.v"); 
     while (getline(genius, line)) {
-        main << line << endl;
+        *file << line << endl;
+        if(line == "//begin1"){
+            *file << "output reg [2:0] State," << endl;
+            *file << "output reg [2:0] Next_state," << endl;
+            *file << "output reg [3:0] Current_level," << endl;
+            *file << "output reg [3:0] Sequence_count," << endl;
+            *file << "output reg [3:0] Current_number," << endl;
+            *file << "//end" << endl;
+        }
+        if(line == "//begin2"){
+            *file << "State <= state;" << endl;
+            *file << "Next_state <= next_state;" << endl;
+            *file << "Current_level <= current_level;" << endl;
+            *file << "Sequence_count <= sequence_count;" << endl;
+            *file << "Current_number <= current_number;" << endl;
+            *file << "//end" << endl;
+        }
     }
     genius.close();
 
     ifstream my_sequence("modules/my_sequence.v");
     while (getline(my_sequence, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     my_sequence.close();
 
     ifstream dec7seg_4bits_1x2("modules/dec7seg_4bits_1x2.v");
     while (getline(dec7seg_4bits_1x2, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     dec7seg_4bits_1x2.close();
 
     ifstream dec7seg_4bits("modules/dec7seg_4bits.v"); 
     while (getline(dec7seg_4bits, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     dec7seg_4bits.close();
 
     ifstream dec7seg_2bits("modules/dec7seg_2bits.v"); 
     while (getline(dec7seg_2bits, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     dec7seg_2bits.close();
 
     ifstream verify_btn("modules/verify_btn.v"); 
     while (getline(verify_btn, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     verify_btn.close();
 
     ifstream recieve_btn_input("modules/recieve_btn_input.v"); 
     while (getline(recieve_btn_input, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     recieve_btn_input.close();
 
     ifstream shift_leds("modules/shift_leds.v"); 
     while (getline(shift_leds, line)) {
-        main << line << endl;
+        *file << line << endl;
     }
     shift_leds.close();
 
     ifstream dec7seg_4bits_hexadec("modules/dec7seg_4bits_hexadec.v");
     while (getline(dec7seg_4bits_hexadec,line)){
-        main << line << endl;
+        *file << line << endl;
+    }
+    dec7seg_4bits_hexadec.close();
+}
+
+void build_file_executable(ofstream* file){
+    string line;
+
+    ifstream genius("modules/genius.v"); 
+    while (getline(genius, line)) {
+        *file << line << endl;
+    }
+    genius.close();
+
+    ifstream my_sequence("modules/my_sequence.v");
+    while (getline(my_sequence, line)) {
+        *file << line << endl;
+    }
+    my_sequence.close();
+
+    ifstream dec7seg_4bits_1x2("modules/dec7seg_4bits_1x2.v");
+    while (getline(dec7seg_4bits_1x2, line)) {
+        *file << line << endl;
+    }
+    dec7seg_4bits_1x2.close();
+
+    ifstream dec7seg_4bits("modules/dec7seg_4bits.v"); 
+    while (getline(dec7seg_4bits, line)) {
+        *file << line << endl;
+    }
+    dec7seg_4bits.close();
+
+    ifstream dec7seg_2bits("modules/dec7seg_2bits.v"); 
+    while (getline(dec7seg_2bits, line)) {
+        *file << line << endl;
+    }
+    dec7seg_2bits.close();
+
+    ifstream verify_btn("modules/verify_btn.v"); 
+    while (getline(verify_btn, line)) {
+        *file << line << endl;
+    }
+    verify_btn.close();
+
+    ifstream recieve_btn_input("modules/recieve_btn_input.v"); 
+    while (getline(recieve_btn_input, line)) {
+        *file << line << endl;
+    }
+    recieve_btn_input.close();
+
+    ifstream shift_leds("modules/shift_leds.v"); 
+    while (getline(shift_leds, line)) {
+        *file << line << endl;
+    }
+    shift_leds.close();
+
+    ifstream dec7seg_4bits_hexadec("modules/dec7seg_4bits_hexadec.v");
+    while (getline(dec7seg_4bits_hexadec,line)){
+        *file << line << endl;
     }
     dec7seg_4bits_hexadec.close();
 
-    main.close();
+
+}
+
+int main(){
+
+    // rodar o programa para """recompilar""" o arquivo main.v
+    // g++ programa_boboca.cpp -o programa_boboca_executavel && ./programa_boboca_executavel
+
+    ofstream main_simulation("test_banch/main_simulation.v");
+    ofstream main_executable("pitanga/main_executable.v");
+    
+    build_file_simulator(&main_simulation);
+    main_simulation.close();
+
+    build_file_executable(&main_executable);
+    main_executable.close();
 
     return EXIT_SUCCESS;
 }
