5d1656bf899e6dea105854d8f9d7bb45b37e326c
Lucas Calumbi <136815529+lucascalumbi@users.noreply.github.com>
2024-10-11 14:39:13 -0300

    Update main_tb.v

--- test_banch/main_tb.v
+++ test_banch/main_tb.v
@@ -11,6 +11,30 @@ module genius_tb;
   wire [6:0] segd3;
   wire [9:0] leds;
   logic [9:2] sw;
+  wire [2:0] State;
+  wire [2:0] Next_state;
+  wire [3:0] Current_level;
+  wire [3:0] Sequence_count;
+  wire [3:0] Current_number;
+
+  // Instância do módulo genius
+  genius genius0(
+    .clock(clock),
+    .btn({btn2, btn1, btn0}),
+    .reset(reset),
+    .start(start), 
+    .sw(sw),
+    .State(State),
+    .Next_state(Next_state),
+    .Current_level(Current_level),
+    .Sequence_count(Sequence_count),
+    .Current_number(Current_number),
+    .segd0(segd0),  
+    .segd1(segd1),  
+    .segd2(segd2),  
+    .segd3(segd3), 
+    .leds(leds)    
+  );
 
   // iverilog -o main_tb.vvp main_tb.v
   // vvp main_tb.vvp
@@ -41,34 +65,25 @@ module genius_tb;
 
     // 2. Iniciar o jogo
     #10 start = 1; 
-    #20 start = 0; 
+    #155 btn2 = 1;
+    #10 btn2 = 0;
+    //#20 start = 0; 
 
     // 3. Simular pressionamento dos botões
-    #10 btn0 = 1; // Pressionar btn0
-    #10 btn0 = 0;  // Soltar btn0
+    // #10 btn0 = 1; // Pressionar btn0
+    // #10 btn0 = 0;  // Soltar btn0
 
-    #10 btn1 = 1; // Pressionar btn1
-    #10 btn1 = 0;  // Soltar btn1
+    // #10 btn1 = 1; // Pressionar btn1
+    // #10 btn1 = 0;  // Soltar btn1
 
-    #10 btn2 = 1; // Pressionar btn2
-    #10 btn2 = 0;  // Soltar btn2
+    // #10 btn2 = 1; // Pressionar btn2
+    // #10 btn2 = 0;  // Soltar btn2
 
     // 4. Simulação finalizada
+
     #10000 $finish;   
   end
 
-  // Instância do módulo genius
-  genius genius0(
-    .clock(clock),
-    .btn({btn2, btn1, btn0}),
-    .reset(reset),
-    .start(start), 
-    .sw(sw),
-    .segd0(segd0),  
-    .segd1(segd1),  
-    .segd2(segd2),  
-    .segd3(segd3), 
-    .leds(leds)    
-  );
+  
 
 endmodule
